b4338034c7f4c65379bfa7238b3d448da7adda6f
Joshua Kang <guppykang@gmail.com>
2020-07-28 21:36:44 +0000

    Do all configs for nautilius pods in one command

--- setup_nautilus.sh
+++ setup_nautilus.sh
@@ -1,3 +1,5 @@
 echo 'export PYTHONPATH="/jbk001-data1/git/SuperPnP:$PYTHONPATH' >> ~/.bashrc
 pip install -r ./TrianFlow/nautilus_requirements.txt
 source ~/.bashrc
+git config --global user.email "guppykang@gmail.com"
+git config --global user.name "Joshua Kang"
