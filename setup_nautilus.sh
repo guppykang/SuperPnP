6614df586e557710cdc46277f46d99342a9e0b85
Joshua Kang <guppykang@gmail.com>
2020-08-08 02:37:33 +0000

    uhh try this new env josh

--- setup_nautilus.sh
+++ setup_nautilus.sh
@@ -1,6 +1,6 @@
 echo 'export PYTHONPATH="/jbk001-data1/git/SuperPnP:$PYTHONPATH"' >> ~/.bashrc
 echo 'export JUPYTER_PATH="/jbk001-data1/git/SuperPnP:$JUPYTER_PATH"' >> ~/.bashrc
-pip install -r ./nautilus_requirements.txt
-source ~/.bashrc
+pip install -r ./requirements_torch.txt
+pip install -r ./requirements.txt
 git config --global user.email "guppykang@gmail.com"
 git config --global user.name "Joshua Kang"
