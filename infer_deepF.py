63c63cfb35f092de3fd3a48a8b5c25b9990e5364
eric-yyjau <eric.yyjau@gmail.com>
2020-10-10 16:15:18 -0700

    clean code and test requirements

--- infer_deepF.py
+++ infer_deepF.py
@@ -24,107 +24,10 @@ from collections import OrderedDict
 from utils.utils import get_configs, vehicle_to_world
 from utils.logging import *
 
-from infer_kitti import infer_vo, save_traj
-from infer_tum import infer_vo_kitti, infer_vo_tum
+from infer_vo import infer_vo, save_traj
+from infer_models import infer_vo_kitti, infer_vo_tum
 
 warnings.filterwarnings("ignore")
-    
-# class infer_deepF(infer_vo): # edited from infer_tum
-#     def __init__(self, seq_id, sequences_root_dir, if_pnp=True):
-#         super().__init__(seq_id, sequences_root_dir, if_pnp)
-        # self.img_dir = sequences_root_dir
-        # #self.img_dir = '/home4/zhaow/data/kitti_odometry/sampled_s4_sequences/'
-        # self.seq_id = seq_id
-        # self.raw_img_h = 480.0 #320
-        # self.raw_img_w = 640.0 #1024
-        # self.new_img_h = 384 #320
-        # self.new_img_w = 512 #1024
-        # self.max_depth = 50.0
-        # self.min_depth = 0.0
-        # self.cam_intrinsics = self.rescale_camera_intrinsics(self.read_calib_file())
-        # self.flow_pose_ransac_thre = 0.1 #0.2
-        # self.flow_pose_ransac_times = 10 #5
-        # self.flow_pose_min_flow = 5
-        # self.align_ransac_min_samples = 3
-        # self.align_ransac_max_trials = 100
-        # self.align_ransac_stop_prob = 0.99
-        # self.align_ransac_thre = 1.0
-        # self.PnP_ransac_iter = 1000
-        # self.PnP_ransac_thre = 1
-        # self.PnP_ransac_times = 5
-        # self.train_sets = [ # only process train_set
-        #     "rgbd_dataset_freiburg3_long_office_household",
-        #     "rgbd_dataset_freiburg3_long_office_household_validation",
-        #     "rgbd_dataset_freiburg3_sitting_xyz",
-        #     "rgbd_dataset_freiburg3_structure_texture_far",
-        #     "rgbd_dataset_freiburg3_structure_texture_near",
-        #     "rgbd_dataset_freiburg3_teddy",
-        #     ]
-        # self.test_sets = [
-        #     "rgbd_dataset_freiburg3_walking_xyz",
-        #     "rgbd_dataset_freiburg3_large_cabinet_validation",
-        #     ]
-    
-    
-    # def read_calib_file(self):
-    #     """ # directly from the website
-    #     https://vision.in.tum.de/data/datasets/rgbd-dataset/file_formats#intrinsic_camera_calibration_of_the_kinect
-
-    #     """
-    #     calib = np.identity(3)
-    #     fu, fv, cu, cv =  535.4, 539.2, 320.1, 247.6
-    #     calib = np.array([[fu, 0, cu], [0, fv, cv], [0, 0, 1]])
-    #     # D = np.array([0,0,0,0,0])
-    #     # height, width, calib, D = self.load_intrinsics(calib_data)
-    #     # calib = proj_c2p[0:3, 0:3]
-    #     # intrinsics_original = calib + 0
-    #     # calib[0,:] *=  zoom_x
-    #     # calib[1,:] *=  zoom_y
-    #     # print(f"calib: {calib}, intrinsics_original: {intrinsics_original}")
-    #     return calib
-    
-    # # @staticmethod
-    # def read_images_files_from_folder(self, path_to_sequence):
-    #     rgb_filenames = []
-    #     timestamps = []
-    #     # path_to_sequence = f"{dataset_dir}/{sequence}"
-    #     with open(f"{path_to_sequence}/rgb.txt") as times_file:
-    #         for line in times_file:
-    #             if len(line) > 0 and not line.startswith('#'):
-    #                 t, rgb = line.rstrip().split(' ')[0:2]
-    #                 rgb_filenames.append(f"{path_to_sequence}/{rgb}")
-    #                 timestamps.append(float(t))
-    #     test_files = rgb_filenames
-    #     timestamps = np.array(timestamps)
-    #     return test_files, timestamps
-    
-    # def load_images(self, max_length=-1):
-    #     print(f'Loading images from sequence {self.seq_id}')
-    #     path = self.img_dir
-    #     seq = self.seq_id
-    #     new_img_h = self.new_img_h
-    #     new_img_w = self.new_img_w
-    #     test_files, timestamps = self.read_images_files_from_folder(f"{path}/{seq}")
-    #     self.timestamps = timestamps
-    #     # seq_dir = os.path.join(path, seq)
-    #     # image_dir = os.path.join(seq_dir, 'image_2')
-    #     num = len(test_files)
-    #     if max_length > 0:
-    #         num = min(int(max_length)+1, num)
-        
-    #     images = []
-    #     for i in tqdm(range(num)):
-    #         image = cv2.imread(test_files[i])
-    #         image = cv2.resize(image, (new_img_w, new_img_h))
-    #         images.append(image)
-
-    #     print('Loaded Images')
-    #     return images
-   
-
-
-
-    
 
 ##### deepF frontend
 from deepFEPE.utils.loader import (
@@ -244,16 +147,6 @@ class deepF_frontend(object):
             logging.debug(f"Rt_cam: {Rt_cam}")
             return Rt_cam
 
-            # pick one of the pose ...
-            # import code; code.interact(local=locals())
-
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     import argparse
@@ -272,9 +165,6 @@ if __name__ == '__main__':
 
     args = arg_parser.parse_args()
     
-   
-
-    # os.environ['CUDA_VISIBLE_DEVICES'] = '0'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logging.info("train on device: %s", device)
 
@@ -353,12 +243,6 @@ if __name__ == '__main__':
     poses = poses[:,:3,:4].reshape(-1, 12)
     print(f'Shape of poses : {poses.shape}')
     vo_test.save_traj(args.traj_save_dir, poses, save_time, args.model)
-
-    # save_time = time.strftime("%Y%m%d-%H%M%S")
-    # poses = poses[:,:3,:4].reshape(-1, 12)
-    # print(f'Shape of poses : {poses.shape}')
-    # vo_test.save_traj(args.traj_save_dir, poses, save_time, args.model)
-    # print(f'Predicted Trajectory saved at : {args.traj_save_dir}/{args.sequence}/{args.model}/preds_{save_time}.txt')
 
 
   
