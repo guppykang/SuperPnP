7ca1258e6a909afbecc7cbf7d2cd442728127afa
Joshua Kang <guppykang@gmail.com>
2020-10-15 05:33:46 +0000

    fixed setu

--- setup_nautilus.sh
+++ setup_nautilus.sh
@@ -1,14 +1,7 @@
 echo 'export PYTHONPATH="/jbk001-data1/git/SuperPnP:$PYTHONPATH"' >> ~/.bashrc
 echo 'export JUPYTER_PATH="/jbk001-data1/git/SuperPnP:$JUPYTER_PATH"' >> ~/.bashrc
-pip install -r ./requirements_torch.txt
-pip install -r ./requirements.txt
-pip install opencv-contrib-python==3.4.2.17
-pip install evo
-pip install tensorboardX
-pip install matplotlib==3.0.3
 git config --global user.email "guppykang@gmail.com"
 git config --global user.name "Joshua Kang"
-pip install pypng
 sudo apt update
 sudo apt install -y tmux
 source ~/.bashrc
